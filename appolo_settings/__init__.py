9c86a85eaba76062a68085d624bd41bb4a523eb3
wangyuxin <wangyuxin@mokahr.com>
2024-03-15 15:55:33 +0800

    Update appolo-settings version to 0.2.0 and fix bug in AppoloSettings.on_change method

--- appolo_settings/__init__.py
+++ appolo_settings/__init__.py
@@ -1,5 +1,4 @@
 from appolo_settings.core import AppoloSettings, AppoloSettingsConfigDict
 from appolo_settings.version import __version__
 
-
 __all__ = ['AppoloSettings', 'AppoloSettingsConfigDict', '__version__']
